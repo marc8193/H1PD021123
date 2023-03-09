da204e7d641c5ca5099e9798f25f5ce580e83073
Marcus Haukelid Larsen <mhl@rtx.dk>
2023-03-09 13:28:04 +0100

    Worked on task

--- Clientsideprogr 2/businesscard-site/script.js
+++ Clientsideprogr 2/businesscard-site/script.js
@@ -6,12 +6,28 @@ document.getElementById("homepage").style.height =  viewPortHeight + "px";
 // Set about page virtual site height
 document.getElementById("about").style.height =  viewPortHeight + "px";
 
+// Auto scroll
 const scrollTrigger = viewPortHeight / 6;
 
+let toTopPressed = false;
+
 window.addEventListener("scroll", (event) => {
-  if (window.scrollY > scrollTrigger) {
+  if (window.scrollY > scrollTrigger && !toTopPressed) {
     document.getElementById("about").scrollIntoView({ 
       behavior: 'smooth'
     });  
   }
+
+  if (window.scrollY < scrollTrigger) {
+    toTopPressed = false;
+  } 
 });
+
+// To top button
+async function toTop() {
+  toTopPressed = true;
+
+  document.getElementById("homepage").scrollIntoView({ 
+    behavior: 'smooth'
+  });
+}
