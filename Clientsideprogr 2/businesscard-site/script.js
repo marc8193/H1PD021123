d93bafdff9c825200c59b41d27dfc803a7c95e43
Marcus Haukelid Larsen <mhl@rtx.dk>
2023-03-09 09:56:10 +0100

    Worked on task

--- Clientsideprogr 2/businesscard-site/script.js
+++ Clientsideprogr 2/businesscard-site/script.js
@@ -6,11 +6,6 @@ document.getElementById("homepage").style.height =  viewPortHeight + "px";
 // Set about page virtual site height
 document.getElementById("about").style.height =  viewPortHeight + "px";
 
-// Allign image vertically
-const imageDivHeight = document.getElementById("image").clientHeight;
-
-document.getElementById("profile").style.marginTop = (imageDivHeight / 2.5) + "px";
-
 const scrollTrigger = viewPortHeight / 6;
 
 window.addEventListener("scroll", (event) => {
