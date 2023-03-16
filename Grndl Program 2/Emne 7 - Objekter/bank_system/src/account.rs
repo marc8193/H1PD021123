92a752eafccea48ddb622c12ed241ee70707f945
Marcus Haukelid Larsen <mhl@rtx.dk>
2023-03-16 13:47:46 +0100

    Added new task

---
+++ Grndl Program 2/Emne 7 - Objekter/bank_system/src/account.rs
@@ -0,0 +1,40 @@
+use std::vec;
+
+use uuid::Uuid;
+use cli_table::{print_stdout, Table, WithTitle};
+
+#[derive(Table, Debug)]
+pub struct Account {
+    #[table(title = "ID")]
+    pub id: Uuid,
+    #[table(title = "Name")]
+    pub name: String,
+    #[table(title = "Balance")]
+    pub balance: i32,
+}
+
+impl Account {
+    pub fn new(name: String) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name,
+            balance: 0,
+        }   
+    }
+
+    pub fn edit(&mut self, value: i32) {
+        self.balance += value;
+    }
+
+    pub fn show(&self) {
+        let account = vec![
+            Self {
+                id: self.id,
+                name: self.name.clone(),
+                balance: self.balance,
+            }
+        ];
+
+        assert!(print_stdout(account.with_title()).is_ok());
+    }
+}
---
+++ Grndl Program 2/Emne 7 - Objekter/bank_system/src/main.rs
@@ -0,0 +1,63 @@
+use inquire::{Text, InquireError, Select};
+use std::str::FromStr;
+
+mod account;
+
+fn select_account(accounts: &Vec<account::Account>) -> usize {
+    let mut options: Vec<&str> = Vec::new();
+
+    for account in accounts {
+        options.push(&account.name);
+    }
+
+    let selected = Select::new("Select account from list:", options).prompt().unwrap_or_default();
+
+    let mut selected_account = 0;
+
+    for account in accounts {
+        if account.name == selected {
+            break;
+        } else {
+            selected_account += 1;
+        }
+    }
+
+    selected_account
+}
+
+fn main() {
+    let mut accounts: Vec<account::Account> = Vec::new();
+
+    loop {
+        let options: Vec<&str> = vec!["Create account", "Withdraw/deposit", "Show balance", "Quit"];
+
+        let answer: Result<&str, InquireError> = Select::new("Select action from list:", options).prompt();
+
+        match answer {
+            Ok(choice) => match choice {
+                "Create account" => {
+                    let name = Text::new("Enter name:").prompt().unwrap_or_default();
+                    
+                    accounts.push(account::Account::new(name));
+                }
+                "Withdraw/deposit" => {
+                    let selected_account = select_account(&accounts);
+                    let selected = Text::new("How much would you like to withdraw/deposit?")
+                        .prompt()
+                        .unwrap_or_default();
+                    
+                    let value: i32 = FromStr::from_str(&selected).unwrap();
+
+                    accounts[selected_account].edit(value);
+                },
+                "Show balance" => {
+                    for account in &accounts {
+                        println!("{:?}", account);
+                    }
+                },
+                _ => break,
+            },
+            Err(_) => println!("There was an error, please try again"),
+        }
+    }
+}
